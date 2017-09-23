168aceb4aa2a2248a0b4adea613516c604e196af
phaffywaffle <dallasmail1@gmail.com>
2017-09-23 16:39:55 -0600

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -14,6 +14,41 @@ err(char* Message, int Code, UINT Behavoir = MB_OK, char* Title = "Error_Placeho
 	exit(Code);
 }
 
+void
+PaintTest(HWND hwnd)
+{
+	
+	PAINTSTRUCT Paint;
+	BeginPaint(hwnd, &Paint);
+	RECT Rect = Paint.rcPaint;
+	HDC DeviceContext = Paint.hdc;
+
+	HBRUSH SolidBrush = CreateSolidBrush(RGB(255, 255, 255));
+	Rectangle(DeviceContext, Rect.left, Rect.top, Rect.right, Rect.bottom);
+
+	SolidBrush = CreateSolidBrush(RGB(255, 0, 0));
+	SelectObject(DeviceContext, SolidBrush);
+	Ellipse(DeviceContext, Rect.left, Rect.top, Rect.right, Rect.bottom);
+	DeleteObject(SolidBrush);
+
+	SolidBrush = CreateSolidBrush(RGB(0, 128, 200));
+	SelectObject(DeviceContext, SolidBrush);
+	Ellipse(DeviceContext, Rect.right / 2 - 9, Rect.bottom / 2 - 9, Rect.right / 2 + 10, Rect.bottom / 2 + 10);
+	DeleteObject(SolidBrush);
+
+	MoveToEx(DeviceContext, Rect.left, Rect.top, 0);
+	LineTo(DeviceContext, Rect.right, Rect.bottom);
+	MoveToEx(DeviceContext, Rect.right, Rect.top, 0);
+	LineTo(DeviceContext, Rect.left, Rect.bottom);
+
+	MoveToEx(DeviceContext, Rect.right / 2, Rect.top, 0);
+	LineTo(DeviceContext, Rect.right / 2, Rect.bottom);
+	MoveToEx(DeviceContext, Rect.left, Rect.bottom / 2, 0);
+	LineTo(DeviceContext, Rect.right, Rect.bottom / 2);
+
+	EndPaint(hwnd, &Paint);
+}
+
 LRESULT CALLBACK
 WindowProc(HWND   hwnd,
 	UINT   uMsg,
@@ -29,26 +64,39 @@ WindowProc(HWND   hwnd,
 	} break;
 	case(WM_PAINT):
 	{
+		//PaintTest(hwnd);
+
 		PAINTSTRUCT Paint;
 		BeginPaint(hwnd, &Paint);
+		RECT Rect = Paint.rcPaint;
 		HDC DeviceContext = Paint.hdc;
 
-		HBRUSH SolidBrush = CreateSolidBrush(RGB(255, 0, 0));
+		HBRUSH SolidBrush = CreateSolidBrush(RGB(255, 255, 0));
 		SelectObject(DeviceContext, SolidBrush);
-		Ellipse(DeviceContext, 0, 0, WindowWidth - 1, WindowHeight - 1);
-		DeleteObject(SolidBrush);
-		
-		SolidBrush = CreateSolidBrush(RGB(0, 128, 200));
-		SelectObject(DeviceContext, SolidBrush);
-		Ellipse(DeviceContext, WindowWidth / 2 - 10, WindowHeight / 2 - 10, WindowWidth / 2 + 10, WindowHeight / 2 + 10);
+		Rectangle(DeviceContext, Rect.left, Rect.top, Rect.right, Rect.bottom);
 		DeleteObject(SolidBrush);
 
-		MoveToEx(DeviceContext, 0, 0, 0);
-		LineTo(DeviceContext, WindowWidth - 1, WindowHeight - 1);
-		MoveToEx(DeviceContext, WindowWidth - 1, 0, 0);
-		LineTo(DeviceContext, 0, WindowHeight - 1);
+		SolidBrush = CreateSolidBrush(RGB(255, 0, 0));
+		POINT Points1[] = {{Rect.left, Rect.top},{Rect.left, Rect.bottom / 2},{Rect.right / 2, Rect.top}};
+		SelectObject(DeviceContext, SolidBrush);
+		Polygon(DeviceContext, Points1, 3);
+		
+		POINT Points2[] = { { Rect.right / 2, Rect.top },{ Rect.right -1, Rect.bottom/ 2 },{ Rect.right - 1, Rect.top } };
+		SelectObject(DeviceContext, SolidBrush);
+		Polygon(DeviceContext, Points2, 3);
+
+		POINT Points3[] = { { Rect.right - 1, Rect.bottom / 2},{ Rect.right / 2, Rect.bottom - 1},{ Rect.right - 1, Rect.bottom - 1} };
+		SelectObject(DeviceContext, SolidBrush);
+		Polygon(DeviceContext, Points3, 3);
+
+		POINT Points4[] = { { Rect.left, Rect.bottom -1 },{ Rect.left, Rect.bottom / 2 },{ Rect.right / 2, Rect.bottom -1} };
+		SelectObject(DeviceContext, SolidBrush);
+		Polygon(DeviceContext, Points4, 3);
+		DeleteObject(SolidBrush);
 
 		EndPaint(hwnd, &Paint);
+		
+		PaintTest(hwnd);
 	} break;
 	default:
 	{
@@ -66,7 +114,7 @@ WinMain(HINSTANCE hInstance,
 {
 	WNDCLASSEX WindowClass = {};
 	WindowClass.cbSize = sizeof(WindowClass);
-	WindowClass.style = CS_DROPSHADOW;
+//	WindowClass.style = CS_DROPSHADOW;
 	WindowClass.lpfnWndProc = WindowProc;
 	WindowClass.hInstance = hInstance;
 	//	WindowClass.hIcon = ;
@@ -78,7 +126,9 @@ WinMain(HINSTANCE hInstance,
 	ScreenHeight = GetSystemMetrics(1);
 	WindowWidth = ScreenWidth / 3;
 	WindowHeight = ScreenHeight / 3;
-	HWND Window = CreateWindowEx(0, "Placeholder_Class_Name", "Placeholder_Title", WS_POPUP | WS_VISIBLE, WindowWidth, WindowHeight, WindowWidth, WindowHeight, 0, 0, hInstance, 0);
+	HWND Window = CreateWindowEx(WS_EX_LAYERED, "Placeholder_Class_Name", "Placeholder_Title", WS_POPUP | WS_VISIBLE, WindowWidth, WindowHeight, WindowWidth, WindowHeight, 0, 0, hInstance, 0);
+	SetLayeredWindowAttributes(Window, RGB(255, 0, 0), 0, LWA_COLORKEY);
+
 	if (!Window) err("Failed to create window", -2);
 
 	Running = true;
